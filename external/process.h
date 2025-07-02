3cce08dca69b25c8bcfab41b50c89d017edf6419
elliot <elliot@ivy.rip>
2025-07-02 18:54:58 -0500

    format

--- external/process.h
+++ external/process.h
@@ -6,8 +6,8 @@
 #include <TlHelp32.h>
 #include <tchar.h>
 
+HANDLE get_proc_handle(const wchar_t *processName);
+
 DWORD get_proc_id(const wchar_t *procName);
 
 uintptr_t get_base_addr(DWORD procId, const wchar_t *modName);
-
-HANDLE get_proc_handle(const wchar_t *processName);
