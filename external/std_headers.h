f5b365de8778c36c21b7c799974a8b80a00776df
elliot <elliot@ivy.rip>
2025-07-07 00:31:56 -0500

    add standard header file
    for buncha windows stuff

---
+++ external/std_headers.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// windows sdk util
+#include <SDKDDKVer.h>
+#include <windows.h>
+#include <TlHelp32.h>
+#include <tchar.h>
+
+// strings + io
+#include <string>
+#include <iostream>
+
+// using
+using std::wcout;
+using std::wcin;
+using std::endl;
+using std::wstring;
