43a6931ac9a5fdeeb0a235e7ec51ac0172b75f3e
elliot <elliot@ivy.rip>
2025-07-23 18:35:18 -0500

    fix health pointer map

--- external/doom/offsets.h
+++ external/doom/offsets.h
@@ -2,5 +2,5 @@
 #include "../memory.h"
 
 const mem::PointerMap ammo_pistol = {0x00A21ED0, {0x438, 0x6D0, 0x618}};
-const mem::PointerMap health = {0x00A21ED0, {0x438, 0x610, 0x298}};
+const mem::PointerMap health = {0x124D5B8, {}};
 const mem::PointerMap armor = {0x00A21ED0, {0x420, 0x618}};
