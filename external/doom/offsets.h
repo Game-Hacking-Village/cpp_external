d9e248be3fea643d82997e81177f37b31408dbd7
elliot <elliot@ivy.rip>
2025-07-31 16:53:11 -0500

    fix doom ammo offset

--- external/doom/offsets.h
+++ external/doom/offsets.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "../memory.h"
 
-const mem::PointerMap ammo_pistol = {0x00A21ED0, {0x438, 0x6D0, 0x618}};
+const mem::PointerMap ammo_pistol = {0x0124D5F0, {0x6D0, 0X618}};
 const mem::PointerMap health = {0x124D5B8, {}};
 const mem::PointerMap armor = {0x00A21ED0, {0x420, 0x618}};
