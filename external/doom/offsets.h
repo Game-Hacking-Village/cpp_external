b55fb772a465149c3765b9ba4fa71cf0b6a996fc
elliot <elliot@ivy.rip>
2025-07-31 18:39:45 -0500

    fix armor pt2

--- external/doom/offsets.h
+++ external/doom/offsets.h
@@ -3,4 +3,4 @@
 
 const mem::PointerMap ammo_pistol = {0x0124D5F0, {0x6D0, 0X618}};
 const mem::PointerMap health = {0x124D5B8, {}};
-const mem::PointerMap armor = {0x0124D5f0, {0x6D0, 0x10, 0x618}};
+const mem::PointerMap armor = {0x1250D88, {0x28, 0x28, 0x618}};
